af1684b9adc3664aa9e7646e18bda07b1a144d7c
Guedes <guedesdeveloper@gmail.com>
2021-07-06 10:04:09 -0300

    Adicionando projeto softplan service

---
+++ src/Services/SoftPlanService/TaxaJurosService.cs
@@ -0,0 +1,13 @@
+﻿using SoftPlanService.Interfaces;
+using System;
+
+namespace SoftPlanService
+{
+    public class TaxaJurosService : ITaxaJurosService
+    {
+        public decimal GetTaxaJuros()
+        {
+            return 0.01M;
+        }
+    }
+}
