468f058089ca5f16126d652e908d5489f20b5e5f
Guedes <guedesdeveloper@gmail.com>
2021-07-06 14:33:25 -0300

    Criação da classe e interface do CalculoJuros

---
+++ src/Services/SoftPlanService/CalculoJurosService.cs
@@ -0,0 +1,52 @@
+﻿using SoftPlanService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftPlanService
+{
+    public class CalculoJurosService : ICalculoJurosService
+    {
+        private readonly ITaxaJurosService _taxaJurosService;
+
+        public CalculoJurosService(ITaxaJurosService taxaJurosService)
+        {
+            _taxaJurosService = taxaJurosService;
+        }
+
+        public decimal CalcularJuros(decimal valorInicial, int meses)
+        {
+            var taxaJuros = _taxaJurosService.GetTaxaJuros();
+
+            ValidarDadosCalculo(valorInicial, meses, taxaJuros);
+
+            return CalcularJurosComposto(Convert.ToDouble(valorInicial), meses, Convert.ToDouble(taxaJuros));
+        }
+
+        private static decimal CalcularJurosComposto(double valorInicial, int meses, double taxaJuros)
+        {
+            var valorFinal  = valorInicial * Math.Pow((1 + taxaJuros), meses);
+
+            return Convert.ToDecimal(valorFinal.ToString("N2"));
+        }
+
+        private static void ValidarDadosCalculo(decimal valorInicial, int meses, decimal taxaJuros)
+        {
+            if (valorInicial == 0)
+                throw new ArgumentOutOfRangeException(nameof(valorInicial), valorInicial, "Valor inicial não pode ser igual a zero");
+            else if (valorInicial < 0)
+                throw new ArgumentOutOfRangeException(nameof(valorInicial), valorInicial, "Valor inicial não pode ser negativo");
+            else if (meses == 0)
+                throw new ArgumentOutOfRangeException(nameof(meses), meses, "Quantidade de meses não pode ser igual a zero");
+            else if (meses < 0)
+                throw new ArgumentOutOfRangeException(nameof(meses), meses, "Quantidade de meses não pode ser negativo");
+            else if (taxaJuros == 0)
+                throw new InvalidOperationException("A Taxa de Juros não pode ser igual a zero");
+            else if (taxaJuros < 0)
+                throw new InvalidOperationException("A Taxa de Juros não pode ser negativo");
+        }
+    }
+}
---
+++ src/Services/SoftPlanService/Interfaces/ICalculoJurosService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftPlanService.Interfaces
+{
+    public interface ICalculoJurosService
+    {
+        decimal CalcularJuros(decimal valorInicial, int meses);
+    }
+}
