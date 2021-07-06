962e79f38b772571c9a438decb619e8756158e38
Guedes <guedesdeveloper@gmail.com>
2021-07-06 10:12:38 -0300

    Criando metodo get da taxa de juros na controller

--- src/API/TaxaJurosAPI/Controllers/TaxaJurosController.cs
+++ src/API/TaxaJurosAPI/Controllers/TaxaJurosController.cs
@@ -1,22 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SoftPlanService.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace TaxaJurosAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[api/controller]")]
     public class TaxaJurosController : ControllerBase
     {       
-
         private readonly ILogger<TaxaJurosController> _logger;
+        private readonly ITaxaJurosService _taxaJurosService;
 
-        public TaxaJurosController(ILogger<TaxaJurosController> logger)
+        public TaxaJurosController(ILogger<TaxaJurosController> logger,
+            ITaxaJurosService taxaJurosService)
         {
             _logger = logger;
+            _taxaJurosService = taxaJurosService;
+        }
+
+        [HttpGet]
+        [Route("taxaJuros")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        public ActionResult Get()
+        {
+            var taxa = _taxaJurosService.GetTaxaJuros();
+
+            return Ok(taxa);
         }
     }
 }
