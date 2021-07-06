1d84dc231d1dea8d3271b415a086dee03425bacf
Guedes <guedesdeveloper@gmail.com>
2021-07-06 18:11:36 -0300

    adicionando projeto de test da TaxaJurosApi

---
+++ test/TaxaJurosAPI.IntegrationTest/TaxaJurosIntegrationTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TaxaJurosAPI.UnitTests
+{
+    public class TaxaJurosIntegrationTest
+    {
+        private readonly HttpClient _client;
+
+        public TaxaJurosIntegrationTest()
+        {
+            var server = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<TaxaJurosAPI.Startup>());
+            _client = server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("GET")]
+        public async Task TaxaJurosGetTestAsync(string method)
+        {
+            // Arrange
+            var request = new HttpRequestMessage(new HttpMethod(method), "/api/TaxaJuros");
+
+            // Act
+            var response = await _client.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("0.01", stringResponse);
+        }
+
+    }
+}
---
+++ test/TaxaJurosAPI.IntegrationTest/TaxaJurosServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using SoftPlanService;
+using SoftPlanService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxaJurosAPI.Controllers;
+using Xunit;
+
+namespace TaxaJurosAPI.UnitTests
+{
+    public class TaxaJurosServiceTest
+    {
+        [Fact(DisplayName = "Taxa de juros retornando com sucesso")]
+        public void CalcularTaxaJuros_Success_Test()
+        {
+            var jurosService = new TaxaJurosService();
+
+
+            var result =  jurosService.GetTaxaJuros();
+
+            Assert.Equal(0.01M, result);
+        }
+
+        [Fact(DisplayName = "Taxa de juros retornando com erro")]
+        public void CalcularTaxaJuros_Error_Test()
+        {
+            var jurosService = new TaxaJurosService();
+
+            var result =  jurosService.GetTaxaJuros();
+
+            Assert.NotEqual(0.02M, result);
+        }
+    }
+}
