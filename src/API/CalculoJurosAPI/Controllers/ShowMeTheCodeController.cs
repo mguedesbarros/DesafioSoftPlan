7302e26e874991215ab2f1876671cc8728d5ee23
Guedes <guedesdeveloper@gmail.com>
2021-07-06 14:53:12 -0300

    Criando controller ShowMeTheCode para retornar url do projeto no git

---
+++ src/API/CalculoJurosAPI/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CalculoJurosAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+        private readonly ILogger<ShowMeTheCodeController> _logger;
+
+        public ShowMeTheCodeController(ILogger<ShowMeTheCodeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public ActionResult Get()
+        {
+            return Ok("https://github.com/mguedesbarros/DesafioSoftPlan");
+        }
+    }
+}
