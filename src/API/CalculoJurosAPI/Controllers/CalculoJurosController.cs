cce16dd399b3316f55ea1142145aee400c357d97
Guedes <guedesdeveloper@gmail.com>
2021-07-06 14:35:20 -0300

    Criando metodo get na controller do CalculoJuros

--- src/API/CalculoJurosAPI/Controllers/CalculoJurosController.cs
+++ src/API/CalculoJurosAPI/Controllers/CalculoJurosController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SoftPlanService.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace CalculoJurosAPI.Controllers
@@ -12,10 +14,24 @@ namespace CalculoJurosAPI.Controllers
     public class CalculoJurosController : ControllerBase
     { 
         private readonly ILogger<CalculoJurosController> _logger;
+        private readonly ICalculoJurosService _calculoJurosService;
 
-        public CalculoJurosController(ILogger<CalculoJurosController> logger)
+        public CalculoJurosController(ILogger<CalculoJurosController> logger,
+            ICalculoJurosService calculoJurosService)
         {
             _logger = logger;
-        }        
+            _calculoJurosService = calculoJurosService;
+        }
+
+        [HttpGet]
+        [Route("valorInicial/{valorInicial}/meses/{meses}")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public ActionResult Get(decimal valorInicial, int meses)
+        {
+            var calculo = _calculoJurosService.CalcularJuros(valorInicial, meses);
+
+            return Ok(calculo);
+        }
     }
 }
