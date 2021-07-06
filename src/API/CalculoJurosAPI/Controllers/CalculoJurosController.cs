ebac73f2f469801ff29d7dc7b4c5d625c80fd47d
Guedes <guedesdeveloper@gmail.com>
2021-07-06 10:16:54 -0300

    Criação da api de calculo de juros

---
+++ src/API/CalculoJurosAPI/Controllers/CalculoJurosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculoJurosAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculoJurosController : ControllerBase
+    { 
+        private readonly ILogger<CalculoJurosController> _logger;
+
+        public CalculoJurosController(ILogger<CalculoJurosController> logger)
+        {
+            _logger = logger;
+        }        
+    }
+}
