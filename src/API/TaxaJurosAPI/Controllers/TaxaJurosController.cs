9ad80c4d1813f3967ec1478a2e38c07de2b9ba09
Guedes <guedesdeveloper@gmail.com>
2021-07-06 09:20:22 -0300

    Criação da api de taxa de juros

---
+++ src/API/TaxaJurosAPI/Controllers/TaxaJurosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxaJurosAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxaJurosController : ControllerBase
+    {       
+
+        private readonly ILogger<TaxaJurosController> _logger;
+
+        public TaxaJurosController(ILogger<TaxaJurosController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
